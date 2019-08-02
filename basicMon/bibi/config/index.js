a461856b0b997234155d131d765c8f05b6740304
yaksun <511809501@qq.com>
2019-08-02 23:28:58 +0800

    node+mongodb+vue基础demo

--- basicMon/bibi/config/index.js
+++ basicMon/bibi/config/index.js
@@ -10,7 +10,18 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+
+      
+      '/api': {
+        target: 'http://localhost:8088/api/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
